45bd75d5d9ad2e73875de31e46d960f01a70503e
ROSPARS Benoit <benoit.rospars@inria.fr>
2018-10-25 10:26:05 +0200

    Update dist

--- dist/bookmark.js
+++ dist/bookmark.js
@@ -35,21 +35,19 @@ javascript:(function () {
   });
   expandables.click();
 
-  console.log(baseUrl);
-
   var w = window.open('');
   w.document.write(
-    '<!DOCTYPE html><html><body onclick="copy()">' +
-    '<h1>Plan :</h1>' +
+    '<!DOCTYPE html><html><body>' +
+    '<h1>Course data :</h1>' +
     '<input id="input"/>' +
-    '<button id="copy" onclick="copy()">Copy</button>' +
-    '<p>Please copy this and paste it in the <a href="https://brospars.github.io/edx-studio-toc/dist/?baseUrl=' + encodeURIComponent(baseUrl) + '">app</a></p>' +
+    '<button id="copy" onclick="copy()">Copy and go to app</button>' +
+    '<p>Click the button, if nothing happen please copy the input content and go to the <a href="https://brospars.github.io/edx-studio-toc/dist/?baseUrl=' + encodeURIComponent(baseUrl) + '">app</a></p>' +
     '<script>' +
       'var input = document.getElementById("input");' +
       'input.value = JSON.stringify(' + JSON.stringify(sections) + ');' +
       'input.focus();' +
       'input.select();' +
-      'function copy () {input.select(); document.execCommand("copy");}' +
+      'function copy () {input.select(); document.execCommand("copy"); window.open("https://brospars.github.io/edx-studio-toc/dist/?baseUrl=' + encodeURIComponent(baseUrl) + '")}' +
     '</script>' +
     '</body></html>'
   );
